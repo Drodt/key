Drodt/key

commit 37d48137259b833f84fd90ce363d72daa3a95e77

Remove remains of Webstart
Java Webstart was removed in Java 11, which is required by KeY.

key.ui/src/main/java/de/uka/ilkd/key/core/Main.java
@@ -539,12 +539,7 @@ public final class Main {
     public static void ensureExamplesAvailable() {
         File examplesDir = getExamplesDir() == null ? ExampleChooser.lookForExamples()
                 : new File(getExamplesDir());
-        if (!examplesDir.exists()) {
-            examplesDir = WebstartMain.setupExamples();
-        }
-        if (examplesDir != null) {
-            setExamplesDir(examplesDir.getAbsolutePath());
-        }
+        setExamplesDir(examplesDir.getAbsolutePath());
     }
 
     private static void updateSplashScreen() {

deployment/KeY.jnlp
@@ -1,34 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!-- JNLP file for KeY -->
-<jnlp spec="1.0+" codebase="https://formal.iti.kit.edu/key/download/nightly/webstart/" href="KeY.jnlp">
-<information> 
-<title>KeY 2.5 Nightly Build</title>
-<vendor>KeY Project, Karlsruhe Institute of Technology, Chalmers
-University, TU Darmstadt</vendor> 
-<homepage href="http://www.key-project.org"/> 
-<description>
-  The KeY tool.
-</description> 
-<icon href="http://www.key-project.org/keylogo.gif"/> 
-<offline-allowed/> 
-</information> 
-<security> 
-<all-permissions/> 
-</security> 
-<resources> 
-<!-- Windows 32-bit -->
-<j2se version="1.7+" initial-heap-size="128m" max-heap-size="1024m" os="Windows" arch="x86"/>
-<j2se version="1.7+" initial-heap-size="128m" max-heap-size="1024m" os="Windows" arch="i386"/>
-<!-- Default -->
-<j2se version="1.7+" initial-heap-size="128m" max-heap-size="2048m"/>
-
-<jar href="components/key.ui.jar" /> 
-<jar href="components/key.core.testgen.jar" /> 
-<jar href="components/key.core.jar" /> 
-<jar href="components/key.util.jar" /> 
-<jar href="libs/antlr.jar" /> 
-<jar href="libs/recoderKey.jar" /> 
-</resources> 
-
-<application-desc main-class="de.uka.ilkd.key.core.WebstartMain"/> 
-</jnlp> 

key.ui/src/main/java/de/uka/ilkd/key/core/WebstartMain.java
@@ -1,63 +1,0 @@
-package de.uka.ilkd.key.core;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-
-import org.key_project.util.java.IOUtil;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-
-public class WebstartMain {
-    private static final Logger LOGGER = LoggerFactory.getLogger(WebstartMain.class);
-
-    private static final int BUFFER_SIZE = 4096;
-
-
-    public static File setupExamples() {
-        try {
-            URL examplesURL = WebstartMain.class.getResource("/examples.zip");
-            if (examplesURL == null) {
-                throw new IOException("Missing examples.zip in resources");
-            }
-
-            File tempDir = createTempDirectory();
-
-            if (tempDir != null) {
-                IOUtil.extractZip(examplesURL.openStream(), tempDir.toPath());
-            }
-            return tempDir;
-        } catch (IOException e) {
-            LOGGER.warn("Error setting up examples", e);
-            return null;
-        }
-    }
-
-
-    private static File createTempDirectory() throws IOException {
-        final File tempDir = File.createTempFile("keyheap-examples-", null);
-        tempDir.delete();
-        if (!tempDir.mkdir()) {
-            return null;
-        }
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> IOUtil.delete(tempDir)));
-        return tempDir;
-    }
-
-
-    public static void main(String[] args) {
-        File examplesDir = setupExamples();
-
-        if (examplesDir != null) {
-            String[] newArgs = new String[args.length + 2];
-            System.arraycopy(args, 0, newArgs, 0, args.length);
-            newArgs[args.length] = "--examples";
-            newArgs[args.length + 1] = examplesDir.getAbsolutePath();
-            args = newArgs;
-        }
-
-        Main.main(args);
-    }
-}

commit d81f31453de49f136d628471e504b8d0f2f86c99

Extract examples when needed
Regression from 37d48137259b833f84fd90ce363d72daa3a95e77

key.ui/src/main/java/de/uka/ilkd/key/core/Main.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -542,7 +543,40 @@ public final class Main {
     public static void ensureExamplesAvailable() {
         File examplesDir = getExamplesDir() == null ? ExampleChooser.lookForExamples()
                 : new File(getExamplesDir());
+        if (!examplesDir.exists()) {
+            examplesDir = setupExamples();
+        }
         setExamplesDir(examplesDir.getAbsolutePath());
+    }
+
+    private static File setupExamples() {
+        try {
+            URL examplesURL = Main.class.getResource("/examples.zip");
+            if (examplesURL == null) {
+                throw new IOException("Missing examples.zip in resources");
+            }
+
+            File tempDir = createTempDirectory();
+
+            if (tempDir != null) {
+                IOUtil.extractZip(examplesURL.openStream(), tempDir.toPath());
+            }
+            return tempDir;
+        } catch (IOException e) {
+            LOGGER.warn("Error setting up examples", e);
+            return null;
+        }
+    }
+
+
+    private static File createTempDirectory() throws IOException {
+        final File tempDir = File.createTempFile("keyheap-examples-", null);
+        tempDir.delete();
+        if (!tempDir.mkdir()) {
+            return null;
+        }
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> IOUtil.delete(tempDir)));
+        return tempDir;
     }
 
     private static void evaluateLemmataOptions(CommandLine options) {