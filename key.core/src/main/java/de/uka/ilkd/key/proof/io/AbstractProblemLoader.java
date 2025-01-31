27d43c2a97a827f6434d99b9afb5f37ed6959553
Richard Bubel <bubel@cs.tu-darmstadt.de>
2025-01-31 18:42:19 +0100

    Remove unnecessarily fully qualified names

--- key.core/src/main/java/de/uka/ilkd/key/proof/io/AbstractProblemLoader.java
+++ key.core/src/main/java/de/uka/ilkd/key/proof/io/AbstractProblemLoader.java
@@ -42,6 +42,9 @@ import org.key_project.util.collection.Pair;
 import org.key_project.util.java.IOUtil;
 
 import org.antlr.runtime.MismatchedTokenException;
+import org.antlr.runtime.MissingTokenException;
+import org.antlr.runtime.RecognitionException;
+import org.antlr.runtime.Token;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -368,10 +371,10 @@ public abstract class AbstractProblemLoader {
     protected ProblemLoaderException recoverParserErrorMessage(Exception e) {
         // try to resolve error message
         final Throwable c0 = unwrap(e);
-        if (c0 instanceof org.antlr.runtime.RecognitionException re) {
-            final org.antlr.runtime.Token occurrence = re.token; // may be null
-            if (c0 instanceof org.antlr.runtime.MismatchedTokenException) {
-                if (c0 instanceof org.antlr.runtime.MissingTokenException mte) {
+        if (c0 instanceof RecognitionException re) {
+            final Token occurrence = re.token; // may be null
+            if (c0 instanceof MismatchedTokenException) {
+                if (c0 instanceof MissingTokenException mte) {
                     // TODO: other commonly missed tokens
                     final String readable = missedErrors.get(mte.expecting);
                     final String token = readable == null ? "token id " + mte.expecting : readable;
@@ -381,7 +384,7 @@ public abstract class AbstractProblemLoader {
                     return new ProblemLoaderException(this, msg, mte);
                     // TODO other ANTLR exceptions
                 } else {
-                    final org.antlr.runtime.MismatchedTokenException mte =
+                    final MismatchedTokenException mte =
                         (MismatchedTokenException) c0;
                     final String genericMsg = "expected " + mte.expecting + ", but found " + mte.c;
                     final String readable =
