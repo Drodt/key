87b7095c0f3ca1a422f2d88370ca2f39fdaaeafc
yseulp <yeseulpark0126@gmail.com>
2026-01-14 10:45:10 +0100

    ghost example

--- keyext.rusty/src/test/resources/testcase/examples/ghost/src/lib.rs
+++ keyext.rusty/src/test/resources/testcase/examples/ghost/src/lib.rs
@@ -3,10 +3,54 @@
 
 extern crate rml_contracts;
 use rml_contracts::*;
-
+/*
 #[spec {ensures(result == 2)}]
-fn foo(a: [i32; 48]) -> i32  {
+fn foo(a:i32) -> i32  {
     let old_a = ghost!{snapshot!(a)};
+    let mut old_b = 1;
+    ghost!{
+        let old_a_a = &old_a;
+
+        };
     let x: i32 = 2;
+    old_b = old_b+1;
     x
+}
+#[spec {ensures(result == 2)}]
+fn foo(a: &mut i32) -> i32  {
+    let tmp: i32 = *a;
+    let old_a = ghost!{snapshot!(tmp)};
+    let one: Ghost<i32> = ghost!{ 1 };
+
+     let now: i32 = tmp + 1;
+     let check = true;
+
+    ghost!{
+        let old = &old_a;
+        check == (old + &one);
+        };
+
+2
+}*/
+
+pub fn foo(a: &mut [i32; 10]){
+    //let tmp: i32 = *a;
+    let old_a = ghost!{snapshot!(a)};
+    let mut i: usize = 0;
+    let len = a.len();
+    let b = a;
+
+    loop{
+        if i >= len {
+            break;
+        }
+        b[i] = b[i] * 2;
+
+        ghost!{
+            let all_doubled = b[i] == (*old_a)[i]*2;
+        };
+
+        i +=1
+    }
+
 }
