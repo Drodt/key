b3724b8680cb5acc58de76ddbe39c2564d862afd
Drodt <daniel.drodt@tu-darmstadt.de>
2026-03-20 12:24:27 +0100

    Fix ProofCollections

--- keyext.rusty/src/test/java/org/key_project/rusty/proof/ProofCollections.java
+++ keyext.rusty/src/test/java/org/key_project/rusty/proof/ProofCollections.java
@@ -36,69 +36,71 @@ public class ProofCollections {
         settings.setVerboseOutput(true);
 
         var c = new ProofCollection(settings);
-        /*
-         * var simple = c.group("simple");
-         * simple.provable("simple.key");
-         * // simple.loadable("man-simple.proof");
-         * simple.provable("if.key");
-         * // simple.loadable("man-if.proof");
-         * simple.provable("iflet.key");
-         * // simple.loadable("man-iflet.proof");
-         * simple.provable("auto-if.key");
-         *
-         * var refs = c.group("references");
-         * refs.provable("shared-ref.key");
-         * // refs.loadable("man-shared-ref.proof");
-         * refs.provable("mutable-ref.key");
-         * // refs.loadable("man-mutable-ref.proof");
-         * refs.notprovable("mutable-ref-wrong.key");
-         * // refs.loadable("man-mutable-ref-wrong.proof");
-         *
-         * var choices = c.group("choices");
-         * choices.provable("sub-no-check.key");
-         * // choices.loadable("man-sub-no-check.proof");
-         *
-         * var contracts = c.group("contracts");
-         * contracts.provable("use-contract.key");
-         * // contracts.loadable("man-use-contract.proof");
-         *
-         * var rustSrc = c.group("rustSrc");
-         * rustSrc.provable("loop-mul.key");
-         * // rustSrc.loadable("man-loop-mul.proof");
-         * rustSrc.provable("add-no-bounds.key");
-         * // rustSrc.loadable("man-add-no-bounds.proof");
-         * rustSrc.provable("mut-ref-src.key");
-         * // rustSrc.loadable("man-mut-ref-src.proof");
-         * rustSrc.provable("if-src.key");
-         * // rustSrc.loadable("man-if-src.proof");
-         *
-         * var array = c.group("array");
-         * array.loadable("array-get-of-repeat.proof");
-         * array.loadable("array-get-of-set.proof");
-         * array.loadable("array-test.proof");
-         * array.loadable("array-enumerate.proof");
-         *
-         * var tuples = c.group("tuples");
-         * tuples.loadable("tuple-test.proof");
-         *
-         * var option = c.group("option");
-         * option.loadable("option.proof");
-         */
+
+        var simple = c.group("simple");
+        simple.provable("simple.key");
+        simple.loadable("man-simple.proof");
+        simple.provable("if.key");
+        simple.loadable("man-if.proof");
+        simple.provable("iflet.key");
+        simple.loadable("man-iflet.proof");
+        simple.provable("auto-if.key");
+
+        var refs = c.group("references");
+        refs.provable("shared-ref.key");
+        refs.loadable("man-shared-ref.proof");
+        refs.provable("mutable-ref.key");
+        refs.loadable("man-mutable-ref.proof");
+        refs.notprovable("mutable-ref-wrong.key");
+        refs.loadable("man-mutable-ref-wrong.proof");
+
+        var choices = c.group("choices");
+        choices.provable("sub-no-check.key");
+        choices.loadable("man-sub-no-check.proof");
+
+        var contracts = c.group("contracts");
+        contracts.provable("use-contract.key");
+        contracts.loadable("man-use-contract.proof");
+
+        var rustSrc = c.group("rustSrc");
+        rustSrc.provable("loop-mul.key");
+        rustSrc.loadable("man-loop-mul.proof");
+        rustSrc.provable("add-no-bounds.key");
+        rustSrc.loadable("man-add-no-bounds.proof");
+        rustSrc.provable("mut-ref-src.key");
+        rustSrc.loadable("man-mut-ref-src.proof");
+        rustSrc.provable("if-src.key");
+        rustSrc.loadable("man-if-src.proof");
+
+        var array = c.group("array");
+        array.provable("array-get-of-repeat.key");
+        array.loadable("man-array-get-of-repeat.proof");
+        array.provable("array-get-of-set.key");
+        array.loadable("man-array-get-of-set.proof");
+        array.provable("array-test.key");
+        array.loadable("man-array-test.proof");
+        array.provable("array-enumerate.key");
+        array.loadable("man-array-enumerate.proof");
+
+        var tuples = c.group("tuples");
+        tuples.provable("tuple-test.key");
+        tuples.loadable("man-tuple-test.proof");
+
+        var option = c.group("option");
+        option.provable("option.key");
+        option.loadable("man-option.proof");
 
         var ghost = c.group("ghost");
         // ghost.provable("double_array_with_ghost.key");
         // ghost.provable("double_array_without_ghost.key");
-
         ghost.provable("reverse_array_with_ghost.key");
         ghost.provable("reverse_array_without_ghost.key");
-
         ghost.provable("if_with_ghost.key");
         ghost.provable("if_without_ghost.key");
-
         // ghost.provable("first-ghost.key");
 
-        // var algos = c.group("algorithms");
-        // algos.provable("binary-search/binary-search.key");
+        var algos = c.group("algorithms");
+        algos.provable("binary-search/binary-search.key");
 
         var fm26 = c.group("fm26");
         fm26.provable("fm26/example1.key");
